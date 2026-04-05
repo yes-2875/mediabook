2c09d67aca57bc058886b120ec51446bd25b803c
yes-2875 <mirzafarrukh97@gmail.com>
2026-04-05 15:07:39 -0400

    First commit - create-next-app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
+import styles from "@/styles/Home.module.css";
 import "@/styles/globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
