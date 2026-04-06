3afc3a584aeaf8878603248f2fc429e893e81a5d
yes-2875 <mirzafarrukh97@gmail.com>
2026-04-06 14:45:31 -0400

    Finish basic navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,24 @@ import { usePathname } from "next/navigation";
 import clsx from 'clsx';
 
 const links = [
-    {key: 1, href: "/"},
-    {key: 2, href: "/"},
+    {key: 1, href: "/", name: "Home"},
+    {key: 2, href: "/about", name: "About"},
 ];
 
 export default function Navbar({ font1, font2 }) {
     const pathName = usePathname();
+    
+    const linkJsx = links.map(link => {
+      return <Link key={link.key} href={link.href}
+        className={clsx(styles.Link, { [styles.current]: pathName === link.href })}>
+        {link.name}
+      </Link>;
+    });
+    
     return (
         <nav className={`${styles.Navbar} ${font1.variable} ${font2.variable}`}>
-            {links.map((link) => {
-                <Link key={link.key} href={link.href}
-                className={clsx(`${styles.Link}`, {'current': pathName === link.href})}>Home</Link>
-            })}
+            <img src='logo_long.svg' className='logo'></img>
+            {linkJsx}
         </nav>
     );
 }
