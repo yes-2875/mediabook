5cd4d0681d53f48ed15662bd82e19b4e83eedd38
yes-2875 <mirzafarrukh97@gmail.com>
2026-04-06 13:49:16 -0400

    Start implementing detection of current link on navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,22 @@
+'use client'
 import Link from "next/link";
 import styles from "@/styles/Navbar.module.css";
+import { usePathname } from "next/navigation";
+import clsx from 'clsx';
+
+const links = [
+    {key: 1, href: "/"},
+    {key: 2, href: "/"},
+];
 
 export default function Navbar({ font1, font2 }) {
+    const pathName = usePathname();
     return (
         <nav className={`${styles.Navbar} ${font1.variable} ${font2.variable}`}>
-            <Link href="/" className={styles.Link}>Home</Link>
+            {links.map((link) => {
+                <Link key={link.key} href={link.href}
+                className={clsx(`${styles.Link}`, {'current': pathName === link.href})}>Home</Link>
+            })}
         </nav>
     );
 }
