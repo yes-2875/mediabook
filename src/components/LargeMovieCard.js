0d2207e198fc5d4d8411db21a814d4c3d676e38e
yes-2875 <mirzafarrukh97@gmail.com>
2026-04-07 16:12:31 -0400

    Implement proper styling of LargeMovieCard component

--- src/components/LargeMovieCard.js
+++ src/components/LargeMovieCard.js
@@ -4,13 +4,17 @@ export default function LargeMovieCard({ movie, isFavourite, setFavourite}) {
     return (
         <div className={styles.LargeMovieCard}>
             <img className={styles.LargeMoviePoster} src={movie.Poster}></img>
-            <h3>{movie.Title}</h3>
-            <h2>{movie.Year}</h2>
-            <p>{movie.Plot}</p>
-            
-            <button onClick={setFavourite}>
-                {isFavourite ? "Remove from Favourites" : "Add to Favourites"}
-            </button>
+            <div className={styles.Content}>
+                <h1>{movie.Title}</h1>
+                <h2>{movie.Year} · {movie.Rated} · {movie.Runtime}</h2>
+                <h3>{movie.Genre} · Rating: {movie.imdbRating} · {movie.imdbVotes} votes</h3>
+                <p>{movie.Plot}</p>
+                <h3>Awards: {movie.Awards}</h3>
+                <h3>Actors: {movie.Actors}</h3>
+                <button className={styles.FavouritesButton} onClick={setFavourite}>
+                    {isFavourite ? "Remove from Favourites" : "Add to Favourites"}
+                </button>
+            </div>
         </div>
     );
 }
