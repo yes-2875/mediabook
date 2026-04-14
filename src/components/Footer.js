28fb4988b9e467b61b34a0dbb6e553c7d820400b
yes-2875 <mirzafarrukh97@gmail.com>
2026-04-14 16:57:05 -0400

    Update footer and add media queries for movie cards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import styles from '@/styles/Footer.module.css';
 export default function Footer({ font1, font2 }) {
     return (
         <footer className={`${styles.Footer} ${font1.variable} ${font2.variable}`}>
-            <p>Movie database is provided courtesy of OMDbapi.com.</p>
+            <p>Movie database is provided courtesy of Themoviedb.org. This product uses the TMDB API but is not certified or endorsed by TMDB.<br></br>
+            Themoviedb.org does not claim ownership of any images or data in the API.
+            </p>
         </footer>
     );
 }
